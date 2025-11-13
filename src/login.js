e2c189e9f083c3928572a9551041d2feb70271d7
Erkhembileg Ariunbold <a.erkhembileg9@gmail.com>
2025-11-14 00:12:51 +0900

    completed design of the login page

--- src/login.js
+++ src/login.js
@@ -2,7 +2,7 @@ const togglePassword = document.getElementById("togglePassword");
 const password = document.getElementById("password");
 const toggle_Lock = document.getElementById("toggleLock");
 const username = document.getElementById("username");
-
+const alertBox = document.getElementById('alert');
 togglePassword.addEventListener("click", function () {
   // Toggle password visibility
   const type =
@@ -19,9 +19,17 @@ function checkPasswordandUsername() {
   const user = username.value;
   if (pass === "HUU" && user === "Eric") {
     toggle_Lock.classList.add("fa-lock-open");
+    alertBox.textContent = "Login Successful";
+    alertBox.className = "success";
     console.log("true");
-    window.location.href = "index.html";
+    setTimeout(() => {
+      alertBox.style.display = 'none';
+      window.location.href = "index.html";
+    }, 800);
   } else {
+    alertBox.textContent = "Incorrect username or password";
+    alertBox.className = 'error';
+    alertBox.style.display = 'block';  // Change the CSS property of the element
     console.log("false");
   }
 }
