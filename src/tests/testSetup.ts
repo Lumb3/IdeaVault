7262293744ecf4b1cf4969543454d449192d2885
Erkhembileg Ariunbold <a.erkhembileg9@gmail.com>
2026-03-12 11:27:37 -0600

    finished up writing unit tests for login page

--- src/tests/testSetup.ts
+++ src/tests/testSetup.ts
@@ -12,14 +12,24 @@ function setupDom() {
     <button id="signupBtn"></button>
   `;
 }
+//** Shape of an authentication API:
+// Must have a function called login
+// Return type – login is a mocked function created by Vitest */
+interface AuthAPI {
+    login: ReturnType<typeof vi.fn>;
+}
+
+declare global {
+    interface Window {
+        authAPI: AuthAPI;
+    }
+}
 
 beforeEach(async () => {
     vi.resetModules();
     setupDom(); // Build the DOM
 
-    (window as any).authAPI = {
-        login: vi.fn()
-    };
+    window.authAPI = { login: vi.fn().mockResolvedValue({ success: false }) };
 
     // stub location (creates a mock DOM location)
     Object.defineProperty(window, "location", {
